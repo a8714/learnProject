4f90aef08b7e2fcc9ab45f9fa4fc4c3b2817a458
roo <871411126@qq.com>
2023-03-12 21:45:05 +0800

    条件查询

---
+++ glkt/service/service_vod/src/main/java/com/atguigu/vod/service/config/VodConfig.java
@@ -0,0 +1,22 @@
+package com.atguigu.vod.service.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author roo
+ * @date 2023/3/12
+ */
+@Configuration
+@MapperScan("com.atguigu.ggkt.vod.mapper")
+public class VodConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
--- glkt/service/service_vod/src/main/java/com/atguigu/vod/service/controller/TeacherController.java
+++ glkt/service/service_vod/src/main/java/com/atguigu/vod/service/controller/TeacherController.java
@@ -2,12 +2,17 @@ package com.atguigu.vod.service.controller;
 
 
 import com.atguigu.ggkt.model.vod.Teacher;
+import com.atguigu.ggkt.vo.vod.TeacherQueryVo;
 import com.atguigu.result.Result;
 import com.atguigu.vod.service.service.TeacherService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -41,6 +46,38 @@ public class TeacherController {
         } else {
             return Result.fail(null);
         }    }
+
+    @ApiOperation(value = "获取分页列表")
+    @PostMapping("{page}/{limit}")
+    public Result index(@PathVariable Long page, @PathVariable Long limit, TeacherQueryVo teacherQueryVo){
+        Page<Teacher> pageParam = new Page<>(page, limit);
+        if (teacherQueryVo==null){
+            Page<Teacher> page1 = teacherService.page(pageParam);
+            return Result.ok(page1);
+        }else {
+            QueryWrapper<Teacher> wrapper = new QueryWrapper<>();
+            String name = teacherQueryVo.getName();//讲师名称
+            Integer level = teacherQueryVo.getLevel();//讲师级别
+            String joinDateBegin = teacherQueryVo.getJoinDateBegin();//开始时间
+            String joinDateEnd = teacherQueryVo.getJoinDateEnd();//结束时间
+            if(!StringUtils.isEmpty(name)) {
+                wrapper.like("name",name);
+            }
+            if(!StringUtils.isEmpty(level)) {
+                wrapper.eq("level",level);
+            }
+            if(!StringUtils.isEmpty(joinDateBegin)) {
+                wrapper.ge("join_date",joinDateBegin);
+            }
+            if(!StringUtils.isEmpty(joinDateEnd)) {
+                wrapper.le("join_date",joinDateEnd);
+            }
+
+            IPage<Teacher> pageModel = teacherService.page(pageParam, wrapper);
+            return Result.ok(pageModel);
+        }
+
+    }
 
 }
 
