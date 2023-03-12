f1c621479c32f6978999756afc8f9a07f538c570
roo <871411126@qq.com>
2023-03-12 19:25:17 +0800

    更新-整合siwage

--- glkt/service/service_vod/src/main/java/com/atguigu/vod/service/ServiceVodApplication.java
+++ glkt/service/service_vod/src/main/java/com/atguigu/vod/service/ServiceVodApplication.java
@@ -3,6 +3,7 @@ package com.atguigu.vod.service;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author roo
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("com.atguigu.vod.service.mapper")
+@ComponentScan("com.atguigu")
 public class ServiceVodApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceVodApplication.class, args);
--- glkt/service/service_vod/src/main/java/com/atguigu/vod/service/controller/TeacherController.java
+++ glkt/service/service_vod/src/main/java/com/atguigu/vod/service/controller/TeacherController.java
@@ -3,11 +3,11 @@ package com.atguigu.vod.service.controller;
 
 import com.atguigu.ggkt.model.vod.Teacher;
 import com.atguigu.vod.service.service.TeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,15 +19,22 @@ import java.util.List;
  * @author roo
  * @since 2023-03-12
  */
+@Api(tags = "讲师操作controller")
 @RestController
 @RequestMapping(value="/admin/vod/teacher")
 public class TeacherController {
     @Autowired
     private TeacherService teacherService;
 
+    @ApiOperation("查询所有讲师")
     @GetMapping("/findAll")
     public List<Teacher> findAll(){
         return teacherService.list();
+    }
+    @ApiOperation("根据id删除讲师")
+    @DeleteMapping("{id}")
+    public boolean removeById(  @PathVariable("id") Long id){
+        return teacherService.removeById(id);
     }
 
 }
